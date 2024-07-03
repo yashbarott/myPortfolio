eb0c00b2ae611c053503acd7de16b3c4ae02a5b7
yashbarott <yashb0227@gmail.com>
2024-07-03 14:31:08 +0530

    Initial commit: Added new project files

--- src/mycomponent.js
+++ src/mycomponent.js
@@ -5,7 +5,8 @@ function MyComponent() {
   return (
     <div className="bg-blue-500 text-white p-4">
       <h1 className="text-2xl font-bold">Welcome to My App</h1>
-      <p className="mt-2">Using Tailwind CSS with React!</p>
+      <p className="mt-2"> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      </p>
     </div>
   );
 }
