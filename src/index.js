144745ca4cdc6f4bf539c0357bd1466ab9b50adb
yashbarott <yashb0227@gmail.com>
2024-07-03 13:57:43 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import MyComponent from './mycomponent';
+import './styles/tailwind.css'; // Path to your tailwind.css file
+import './index.css'; // Optional: Import your CSS styles here
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <MyComponent />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/mycomponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/tailwind.css';
+
+function MyComponent() {
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      <h1 className="text-2xl font-bold">Welcome to My App</h1>
+      <p className="mt-2">Using Tailwind CSS with React!</p>
+    </div>
+  );
+}
+
+export default MyComponent;
